b25b9c141c1a2e2d70adce1094382ec56c3a5224
DUzzL <98381970+DUzzL@users.noreply.github.com>
2026-07-12 15:20:49 +0200

    Delete TradeWatch 26.2 directory

--- TradeWatch 26.2/src/client/java/dev/duzzl/tradewatch/trade/TradeMatch.java
+++
@@ -1,5 +0,0 @@
-package dev.duzzl.tradewatch.trade;
-
-import net.minecraft.world.item.enchantment.Enchantment;
-
-public record TradeMatch(int offerIndex, String enchantmentId, Enchantment enchantment, int level, int emeraldPrice) { }
